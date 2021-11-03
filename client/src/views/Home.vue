dcefa6a92de369b2f26c4fa8c6a30a1fe2ca353b
Craig Worthington <craigworthington33@gmail.com>
2021-11-03 15:27:50 -0400

    Added naive-ui as dependency, tested out radio buttons

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <ToolPalette />
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,11 +9,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ToolPalette from '@/components/ToolPalette.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    ToolPalette,
   },
 });
 </script>
