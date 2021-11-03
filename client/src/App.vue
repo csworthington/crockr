e8158f9cf257b2972b96e0cce5616b599ce1593a
Tolu <toluwalopeajisola@gmail.com>
2021-11-03 12:51:20 -0500

    router link to canvas

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/canvas">Canvas</router-link>
   </div>
-  <p> "Hello"</p>
   <router-view/>
 </template>
 
