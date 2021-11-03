7884b00a03f094bc7cc089d862c7c3688d2053e7
Craig Worthington <craigworthington33@gmail.com>
2021-11-03 16:17:45 -0400

    added fabric, fixed dependencies for naive-ui

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/canvaspage',
+    name: 'Canvas Page',
+    component: () => import('../views/CanvasPage.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ client/src/views/CanvasPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="canvas-background-box">
+    test test
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { NLayout, NCard } from 'naive-ui';
+
+export default defineComponent({
+  components: {
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+.box {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+.canvas-background {
+  display: flex;
+  flex-flow: column;
+  background-color: gray;
+  height: 100%;
+}
+</style>
