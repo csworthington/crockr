a09709bed93d126c95e9ac7a36af6c052d2854dd
Craig Worthington <craigworthington33@gmail.com>
2021-11-04 16:01:59 -0400

    fixed issue with typecasting being detected as jsx

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: false,
+    },
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
