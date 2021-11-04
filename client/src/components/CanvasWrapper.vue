b8731d342105f4c7a8a7679b5ddd168a72accd61
csworthington <craigworthington33@gmail.com>
2021-11-04 17:03:05 -0400

    Adding Fabric.js canvas with some basic tools to the project (#10)
    * added fabric. working on fixing a bug with eslint
    
    * fixed eslint parsing error when jsx was enabled
    
    * removed test text from canvaswrapper
    
    * working on adding pen tool button to canvas
    
    * added basic pen tool
    
    * cleaned up code, set up width of canvas to fit to div
    
    * router link to canvas
    
    * added erase button
    
    * rectangle button
    
    Co-authored-by: Tolu <toluwalopeajisola@gmail.com>
    Co-authored-by: dkout <DimitryKoutchine@cmail.carleton.ca>

---
+++ client/src/components/CanvasWrapper.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="canvas-wrapper-div" class="canvas-border">
+    <canvas id="main-canvas"></canvas>
+  </div>
+  <div>
+    <span><button @click="togglePenTool">Pen tool toggle</button></span><span>{{ penStatus }}</span>
+    <span><button @click="rectangle"> Rectangle </button></span>
+    <span><button @click="select"> select </button></span>
+    <span><button @click="erase"> Erase </button></span>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent, onBeforeMount, onMounted, reactive, Ref, ref,
+} from 'vue';
+import { fabric } from 'fabric';
+
+export default defineComponent({
+  name: 'CanvasWrapper',
+
+  setup(props) {
+    let canvasData: fabric.Canvas = reactive((<fabric.Canvas> {}));
+
+    const penStatus: Ref<boolean> = ref(false);
+
+    const togglePenTool = async () => {
+      penStatus.value = !penStatus.value;
+      canvasData.isDrawingMode = penStatus.value;
+      console.log(`pen tool is now ${penStatus.value}`);
+    };
+
+    const rectangle = async () => {
+      canvasData.clear();
+    };
+
+    const erase = async () => {
+      canvasData.clear();
+    };
+
+    /**
+     * Initialize the Fabric.js canvas
+     */
+    const initFabricCanvas = async () => {
+      // Get width for new canvas from wrapper div and set it when creating new canvas
+      // Note: this is not responsive and will not resize canvas element when page is resized
+      const canvasDiv: HTMLDivElement = (<HTMLDivElement> document.getElementById('canvas-wrapper-div'));
+
+      canvasData = new fabric.Canvas('main-canvas', {
+        width: canvasDiv.clientWidth,
+        height: canvasDiv.clientHeight,
+      });
+
+      // Set Drawing mode
+      canvasData.isDrawingMode = false;
+    };
+
+    // When component is mounted, run initFabricCanvas
+    onMounted(initFabricCanvas);
+
+    return {
+      penStatus,
+      canvasData,
+      togglePenTool,
+      rectangle,
+      erase,
+      initFabricCanvas,
+    };
+  },
+});
+</script>
+
+<style>
+.canvas-border {
+  border: 1px solid black;
+}
+</style>
