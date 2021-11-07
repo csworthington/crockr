1f9f04864128beb7320ee918d324f49e0ebd60d8
dkout <DimitryKoutchine@cmail.carleton.ca>
2021-11-07 12:01:38 -0500

    Added line tool, erase selecected object, and a confirm for clearing the board

--- client/src/components/CanvasWrapper.vue
+++ client/src/components/CanvasWrapper.vue
@@ -6,7 +6,8 @@
     <span><button @click="togglePenTool">Pen tool toggle</button></span><span>{{ penStatus }}</span>
     <span><button @click="rectangle"> Rectangle </button></span>
     <span><button @click="select"> select </button></span>
-    <span><button @click="erase"> Erase </button></span>
+    <span><button @click="clearBoard"> Clear </button></span>
+    <span><button @click="lineTool"> Line Tool </button></span>
   </div>
 </template>
 
@@ -21,9 +22,18 @@ export default defineComponent({
 
   setup(props) {
     let canvasData: fabric.Canvas = reactive((<fabric.Canvas> {}));
-
+    let lineStatus = false;
+    let firstCoord = false;
+    const color = 'black';
+    let lineFirstCoord = [1, 2];
     const penStatus: Ref<boolean> = ref(false);
-
+    const lineTool = async () => {
+      if (lineStatus === false) {
+        lineStatus = true;
+      } else {
+        lineStatus = false;
+      }
+    };
     const togglePenTool = async () => {
       penStatus.value = !penStatus.value;
       canvasData.isDrawingMode = penStatus.value;
@@ -33,11 +43,15 @@ export default defineComponent({
     const rectangle = async () => {
       canvasData.clear();
     };
-
-    const erase = async () => {
-      canvasData.clear();
+    const deleteSelected = async () => {
+      canvasData.remove(canvasData.getActiveObject());
     };
 
+    const clearBoard = async () => {
+      if (window.confirm('Are you sure you want to clear the canvas?')) {
+        canvasData.clear();
+      }
+    };
     /**
      * Initialize the Fabric.js canvas
      */
@@ -50,24 +64,56 @@ export default defineComponent({
         width: canvasDiv.clientWidth,
         height: canvasDiv.clientHeight,
       });
-
       // Set Drawing mode
       canvasData.isDrawingMode = false;
+      // Canvas events
+      canvasData.on('mouse:down', async (event) => {
+        const mouseX = canvasData.getPointer(event.e).x;
+        const mouseY = canvasData.getPointer(event.e).y;
+        if (lineStatus === true) {
+          console.log('test');
+          if (firstCoord === false) {
+            lineFirstCoord = [mouseX, mouseY];
+            firstCoord = true;
+          } else {
+            canvasData.add(new fabric.Line([lineFirstCoord[0], lineFirstCoord[1], mouseX, mouseY], {
+              stroke: color,
+              strokeWidth: 10,
+            }));
+            firstCoord = false;
+          }
+        }
+      });
+      canvasData.on('selection:created', async () => {
+        console.log('selection created');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerHTML = 'delete selected element';
+        deleteBtn.id = 'deleteBtn';
+        deleteBtn.onclick = deleteSelected;
+        document.body.appendChild(deleteBtn);
+      });
+      canvasData.on('selection:cleared', async () => {
+        console.log('selection cleared');
+        const elem = document.getElementById('deleteBtn');
+        if (elem != null) {
+          elem.remove();
+        }
+      });
     };
-
     // When component is mounted, run initFabricCanvas
     onMounted(initFabricCanvas);
-
     return {
       penStatus,
       canvasData,
       togglePenTool,
       rectangle,
-      erase,
+      clearBoard,
       initFabricCanvas,
+      lineTool,
     };
   },
 });
+
 </script>
 
 <style>
