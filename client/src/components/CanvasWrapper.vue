33fb06a399bd7ff6688df5983f45aebedd831137
dkout <DimitryKoutchine@cmail.carleton.ca>
2021-11-18 13:16:52 -0500

    added fill

--- client/src/components/CanvasWrapper.vue
+++ client/src/components/CanvasWrapper.vue
@@ -154,7 +154,7 @@ export default defineComponent({
         radius: 1,
         strokeWidth: 2,
         stroke: store.state.primaryToolColour,
-        fill: '',
+        fill: 'store.state.primaryToolColour',
         originX: 'center',
         originY: 'center',
       });
