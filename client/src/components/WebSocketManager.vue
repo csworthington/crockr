0d5ad5173ce959aa02cf84d4467b87a75894dc5d
csworthington <craigworthington33@gmail.com>
2021-11-21 16:44:21 -0500

    created websocketmanager component, and page for testing

---
+++ client/src/components/WebSocketManager.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const socket = new WebSocket('ws://localhost:3000');
+
+    socket.addEventListener('open', (evt: Event) => {
+      console.log('a connection has been opened');
+    });
+    return {
+      socket,
+    };
+  },
+});
+</script>
---
+++ client/src/views/WebSockets.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="websockets">
+    This is the websockets page
+    <WebSocketManager/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import WebSocketManager from '@/components/WebSocketManager.vue';
+
+export default defineComponent({
+  components: {
+    WebSocketManager,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
