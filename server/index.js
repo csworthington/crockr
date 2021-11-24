ae33d6021ec116eb0bfe6bd0eb5a7c90d81a21d2
Tolu <toluwalopeajisola@gmail.com>
2021-11-24 15:54:29 -0600

    delete later

---
+++ server/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ws = require("ws");
+
+const client = new ws("ws://localhost:3000");
+
+client.on("open", () => {
+    // Causes the server to print "Hello"
+    client.send("Hello");
+});
---
+++ server/server.js
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// import WebSocket from "ws";
+
+const express = require("express");
+const ws = require("ws");
+
+const app = express();
+
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on("connection", socket => {
+    socket.on("message", message => console.log(message));
+});
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+const server = app.listen(3000);
+server.on("upgrade", (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit("connection", socket, request);
+    });
+});
+
+/*
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+}); */
