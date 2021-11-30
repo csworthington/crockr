1331081d20d9314cd1f4b42b410a9d363b1c73ed
csworthington <craigworthington33@gmail.com>
2021-11-30 13:49:55 -0500

    fixed incorrect axios injection

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,11 +4,12 @@ import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { axiosInjectionKey } from './symbols';
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(VueAxios, axios);
-app.provide('axios', app.config.globalProperties.axios); // Provide axios globally
+app.provide(axiosInjectionKey, app.config.globalProperties.axios); // Provide axios globally
 
 app.mount('#app');
