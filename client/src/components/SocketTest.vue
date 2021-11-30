048f755058e1da6fc05fcdade84cd78cfd852e1b
Tolu <toluwalopeajisola@gmail.com>
2021-11-30 12:34:45 -0600

    k

--- client/src/components/SocketTest.vue
+++ client/src/components/SocketTest.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="app">
-  My name is <input v-model="message" placeholder="edit me">
-<p>Message is: {{ message }}</p>
+  <p>
+    My name is <input v-model="name">
+  </p>
+<p>
+    <button @click="save">Save</button>
+  </p>
 </div>
   <router-view/>
 </template>
+
+<script lang="ts">
+/* eslint-disable no-shadow */
+import { v4 as uuidv4 } from 'uuid';
+import { computed, defineComponent, WritableComputedRef } from 'vue';
+
+export default defineComponent({
+  setup() {
+    function save() {
+      const myuuid = uuidv4();
+
+      console.log(`Your UUID is: ${myuuid}`);
+    }
+    return {
+      save,
+    };
+  },
+});
+
+</script>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // import WebSocket from "ws";
-
+/*
 const express = require("express");
 const ws = require("ws");
 
@@ -31,16 +31,16 @@ server.on("upgrade", (request, socket, head) => {
         wsServer.emit("connection", socket, request);
     });
 });
+*/
 
-/*
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/games", (req, res) => {
     res.send("Hello World!");
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-}); */
+});
