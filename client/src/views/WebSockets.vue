287691b0904a6ef351d287b4919af04d0011441a
csworthington <craigworthington33@gmail.com>
2021-12-02 11:55:01 -0500

    creating better chat app

--- client/src/views/WebSockets.vue
+++ client/src/views/WebSockets.vue
@@ -3,6 +3,9 @@
     This is the websockets page
     <WebSocketStatusIndicator/>
   </div>
+  <div>
+    <MessageComposer />
+  </div>
   <!-- <WebSocketManager/> -->
   <SocketManager />
 </template>
@@ -12,12 +15,14 @@ import { defineComponent } from 'vue';
 
 import SocketManager from '@/components/websockets/SocketManager.vue';
 import WebSocketStatusIndicator from '@/components/websockets/WebSocketStatusIndicator.vue';
+import MessageComposer from '@/components/ChatPage/MessageComposer.vue';
 
 export default defineComponent({
   components: {
     // WebSocketManager,
     SocketManager,
     WebSocketStatusIndicator,
+    MessageComposer,
   },
   setup() {
     return {};
