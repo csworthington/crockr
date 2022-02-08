f341f6ac289e4ea6ab35aac7030bf201b6e0a608
csworthington <craigworthington33@gmail.com>
2022-02-08 13:18:20 -0500

    working on getting mathjax to work

--- client/src/components/EquationViewer.vue
+++ client/src/components/EquationViewer.vue
@@ -1,17 +1,50 @@
 <template>
   <h1> This is the equation viewer </h1>
+  <textarea id="tex-equation" v-model.trim="texEquation" />
+  <button id="process-equation">Process</button>
+  <div id="output"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, Ref, ref } from 'vue';
 
-const { mathjax } = require('mathjax-full/js/mathjax.js');
+const PACKAGES = 'base, autoload, require, ams, newcommand';
+
+require.context();
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const MathJaxAPI = require('mathjax-full').init({
+  options: { enableAssistiveMml: true },
+  loader: {
+    source: {},
+    load: ['adaptors/liteDOM', 'tex-svg'],
+  },
+  packages: PACKAGES.split(/\s*,\s*/),
+  svg: {
+    fontCache: 'local',
+  },
+  startup: {
+    typeset: false,
+  },
+}).then((MathJax: any) => {
+  // const svg = MathJax.tex2svg();
+  const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', { display: true });
+  console.log(MathJax.startup.adaptor.outerHTML(svg));
+});
 
 export default defineComponent({
   setup() {
-    console.log(MathJax);
+    const texEquation: Ref<string> = ref('E = mc^2');
 
-    return {};
+    // if (window.MathJax) {
+    //   const convertTex = () => {
+    //     const input = texEquation.value;
+    //   };
+    // }
+
+    return {
+      texEquation,
+    };
   },
 });
 </script>
