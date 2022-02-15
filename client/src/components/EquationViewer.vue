7df3490955e940ff907956d44d909e521a5fbe00
csworthington <craigworthington33@gmail.com>
2022-02-15 11:44:05 -0500

    rendering equation with katex and html2canvas now

--- client/src/components/EquationViewer.vue
+++ client/src/components/EquationViewer.vue
@@ -1,72 +1,52 @@
 <template>
   <h1> This is the equation viewer </h1>
-  <textarea id="tex-equation" v-model.trim="texEquation" />
-  <button id="process-equation" @click="processEquation">Process</button>
-  <div id="mathjax-output" ></div>
+  <textarea id="tex-equation" v-model.trim="texEquation" @input="renderEquationToHTML"/>
+  <div>
+    <button id="process-equation" @click="processEquation">Process</button>
+    <button id="render-equation-canvas" @click="printCanvasFromKatex">Print Canvas</button>
+  </div>
+  <div id="katex-output" ></div>
+  <div id="html-to-canvas-div"></div>
 </template>
 
 <script lang="ts">
 import {
-  defineComponent, reactive, Ref, ref,
+  defineComponent, Ref, ref,
 } from 'vue';
-import { initMathJax } from '@/services/mathjax/mathjax';
-
-function mjCallback() {
-  console.log('mathjax callback');
-}
-
-initMathJax(mjCallback);
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const MathJaxAPI = require('mathjax-full').init({
-//   options: { enableAssistiveMml: true },
-//   loader: {
-//     source: {},
-//     load: ['adaptors/liteDOM', 'tex-svg'],
-//   },
-//   packages: PACKAGES.split(/\s*,\s*/),
-//   svg: {
-//     fontCache: 'local',
-//   },
-//   startup: {
-//     typeset: false,
-//   },
-// }).then((MathJax: any) => {
-//   // const svg = MathJax.tex2svg();
-//   const svg = MathJax.tex2svg('\\frac{1}{x^2-1}', { display: true });
-//   console.log(MathJax.startup.adaptor.outerHTML(svg));
-// });
+import katex from 'katex';
+import html2canvas from 'html2canvas';
 
 export default defineComponent({
   setup() {
     const texEquation: Ref<string> = ref('E = mc^2');
-    const svgOutput: any = reactive({});
 
-    const processEquation = () => {
-      (window as any).MathJax.texReset();
-      const options = (window as any).MathJax.getMetricsFor(texEquation.value);
-      options.display = true;
+    const renderEquationToHTML = () => {
+      const katexDiv = document.getElementById('katex-output');
 
-      // const svg = (window as any).MathJax.tex2svg(texEquation.value);
+      if (katexDiv) {
+        katex.render(texEquation.value, katexDiv, {
+          throwOnError: false,
+        });
+      }
+    };
 
-      (window as any).MathJax.tex2svgPromise(texEquation.value, options).then((node: any) => {
-        const mathjaxDiv = document.getElementById('mathjax-output');
-        if (mathjaxDiv) {
-          mathjaxDiv.appendChild(node);
-        }
-        (window as any).MathJax.startup.document.clear();
-        (window as any).MathJax.startup.document.updateDocument();
-      });
-      // console.log(typeof svg);
-      // svgOutput.value = svg;
-      // console.log(svgOutput.value);
-      // svgOutput.value = '<p>Goodbye</p>';
+    const printCanvasFromKatex = () => {
+      const katexDiv = document.getElementById('katex-output');
+
+      if (katexDiv) {
+        html2canvas(katexDiv).then((canvas) => {
+          const canvasDiv = document.getElementById('html-to-canvas-div');
+          if (canvasDiv) {
+            canvasDiv.appendChild(canvas);
+          }
+        });
+      }
     };
 
     return {
       texEquation,
-      svgOutput,
-      processEquation,
+      renderEquationToHTML,
+      printCanvasFromKatex,
     };
   },
 });
