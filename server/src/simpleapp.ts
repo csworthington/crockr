c8a1ce90b3c426e8799dd882bf564320067f1a00
Tolu <toluwalopeajisola@gmail.com>
2022-02-15 20:43:00 -0500

    initial cookie

--- server/src/simpleapp.ts
+++ server/src/simpleapp.ts
@@ -3,12 +3,29 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import flash from "express-flash";
 import lusca from "lusca";
+import mongoose from "mongoose";
+import passport from "passport";
+import bluebird from "bluebird";
+import MongoStore from "connect-mongo";
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 
 import * as uuidController from "./controllers/uuid";
 import * as canvasController from "./controllers/canvas/canvas";
+import session from "express-session";
 
 // Create the Express server
 const app = express();
+// Connect to MongoDB
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoUrl).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch(err => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+});
+
 
 // Set up configuration for Express
 app.set("port", process.env.PORT || 3000);
@@ -18,6 +35,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+        mongoUrl,
+        mongoOptions: {
+            autoReconnect: true
+        }
+    }),
+    cookie: { maxAge: 60000 }
+}));
 
 /**
  * Set up canvas
