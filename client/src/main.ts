5ef997bd20f270950eb2caa1d30ad8df53f9017b
csworthington <craigworthington33@gmail.com>
2022-02-25 11:33:29 -0500

    Added bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,6 +8,9 @@ import { axiosInjectionKey, StoreKey } from './symbols';
 import GlobalSocket from '@/plugins/websocket/GlobalWebSocket';
 import addCustomFabricObjects from '@/utils/addCustomFabricObjects';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 import 'katex/dist/katex.min.css';
 
 // Ensure that all custom fabric objects are added to the global fabric object
